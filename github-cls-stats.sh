fd428f9c2e51ee76725555d852d49a53fbff2ebe
Yixin Shi <yxshi@google.com>
2024-07-12 17:23:32 +0000

    Updated with new author

--- github-cls-stats.sh
+++ github-cls-stats.sh
@@ -5,13 +5,14 @@
 function find_prs_by_author() {
     local repo="$1"
     local author="$2"
+    sleep 1
 
     api_url="https://api.github.com/repos/$repo/pulls?state=all&per_page=100"
     prs=$(curl -s "$api_url" | jq -r '.[] | select(.user.login == "'"$author"'") | [.title, .html_url, .created_at] | @tsv')
 
-    if [ -z "$prs" ]; then
-        echo "No PRs found for author '$author' in repository '$repo'."
-    else
+    if [ -n "$prs" ]; then
+        #echo "No PRs found for author '$author' in repository '$repo'."
+    #else
         echo "### PRs by '$author' in '$repo':"
         echo "$prs" | while IFS=$'\t' read -r title pr_url created_at; do
             echo "- **[$title]($pr_url)** (Created at: $created_at)"
@@ -19,15 +20,15 @@ function find_prs_by_author() {
     fi
 }
 # Hardcoded repository list
-repos=("pytorch/xla" "tensorflow/tensorflow" "google/JetStream")
+repos=("pytorch/xla" "tensorflow/tensorflow" "google/JetStream" "tensorflow/recommenders" "tensorflow/models" "GoogleCloudPlatform/ml-auto-solutions")
 
 # Hardcoded author list
-authors=("will-cromar" "JoeZijunZhou" "qihqi")
+authors=("will-cromar" "richardsliu" "qihqi" "ZhaoyueCheng" "wenxindongwork")
 
 # Iterate through repositories and authors
 for repo in "${repos[@]}"; do
     for author in "${authors[@]}"; do
         find_prs_by_author "$repo" "$author"
-        echo "---------------------------"
+        #echo "---------------------------"
     done
 done
